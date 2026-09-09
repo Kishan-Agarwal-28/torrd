82672e27398b5258d46c660d93f5fe76b0a19b53
Kishan Agarwal <kishanagarwal028@gmail.com>
2026-09-09 13:19:11 +0530

    fix:GPG KEY

--- torrd.sh
+++ torrd.sh
@@ -254,7 +254,7 @@ if $SKIP_TOR_REPO; then
 else
     # Import Tor Project signing key
     KEYRING="/usr/share/keyrings/tor-archive-keyring.gpg"
-    wget -qO- "https://deb.torproject.org/torproject.org/A3C4F.F979CAA22CDBA8F512EE8CBC9E886DDD89.asc" \
+    wget -qO- "https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc" \
       | gpg --dearmor | tee "$KEYRING" >/dev/null
 
     # Write sources.list entry
